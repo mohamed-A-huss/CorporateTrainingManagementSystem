bc2a0ea2bda3e686686aad24a35a458e37314043
Mat <Mat@DESKTOP-90JQGSU>
2026-07-03 14:45:42 +0300

    Department

---
+++ CorporateTrainingManagementSystem/Areas/Admin/Controllers/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorporateTrainingManagementSystem.Areas.Admin.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
