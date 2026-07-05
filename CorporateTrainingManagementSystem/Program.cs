0678eae4d2f2d72301214b65d95bfc8972415a2a
Ali7Amera <alsoultanameri22448866@gmail.com>
2026-07-05 22:29:48 +0300

    coment

--- CorporateTrainingManagementSystem/Program.cs
+++ CorporateTrainingManagementSystem/Program.cs
@@ -38,7 +38,7 @@ namespace CorporateTrainingManagementSystem
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            // Ali 
             app.UseHttpsRedirection();
             app.UseRouting();
 
